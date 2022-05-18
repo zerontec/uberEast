5c98b9210b8bce499afef50bd23af3fd207a9156
zerontec <zerontecnologia1@gmail.com>
2022-05-17 22:09:46 -0400

    add Home.js

---
+++ screems/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import HeaderTabs from '../components/HeaderTabs';
+
+
+
+
+export default function (){
+
+return(
+<SafeAreaView>
+
+<HeaderTabs/>
+
+
+</SafeAreaView>
+
+
+)
+
+
+}
